d9b494da7048ce563db7c63ab2a91efb363ee399
XpyCTuk <gon4arov@gmail.com>
2021-09-10 14:14:03 +0300

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -70,14 +70,121 @@ $(() => {
 
   $('.modale-button').on('click', () => {
     $('.modal-form').addClass('modal-form-open');
-
   })
+
+  $('#form').on('submit', function (event) {
+    event.preventDefault()
+    let name = $('#name').val().trim();
+    let phone = $('#phone').val().trim();
+    let email = $('#email').val().trim();
+    console.log(name);
+    const createHtmlForEmail = () => {
+      return `<div>
+        <div>
+          name: <b>${name}</b>
+        </div>
+        <div>
+          phone: <b>${phone}</b>
+        </div>
+        <div>
+          email: <b>${email}</b>
+        </div>
+        </div>`
+    }
+
+    // if (name == '') {
+    //   $('#errorMassage').text("Введите имя")
+    //   return false;
+    // } else if (contact == '') {
+    //   $('#errorMassage').text("Введите номер телефона")
+    //   return false;
+    // }
+    // $('#errorMassage').text('')
+
+    const letterData = {
+      to: 'goncharovfigma@gmail.com',
+      // to: 'justicejesus1237@gmail.com',
+      subject: 'Форма "Заказать проект"',
+      text: 'yo',
+      html: createHtmlForEmail()
+    }
+    $.ajax({
+      url: 'https://api.42.works/mailer',
+      type: 'POST',
+      //cache: false,
+      data: JSON.stringify(letterData),
+      beforeSend: function () {
+        $('#sendForm').prop("disabled", true)
+      },
+      success: function (data) {
+        if (!data) {
+          alert('Произошла ошибка')
+        } else {
+          $('#form').trigger("reset")
+        }
+        $('#sendForm').prop("disabled", false)
+      },
+      contentType: "application/json; charset=utf-8",
+    });
+  })
+
+  //вторая форма 
+
+  $('#form2').on('submit', function (event) {
+    event.preventDefault()
+    let name2 = $('#name2').val().trim();
+    let contact2 = $('#phone2').val().trim();
+    let email2 = $('#email2').val().trim();
+    const createHtmlForEmail2 = () => {
+      return `<div>
+      <div>
+        name: <b>${name2}</b>
+      </div>
+      <div>
+        phone: <b>${contact2}</b>
+      </div>
+      <div>
+        email: <b>${email2}</b>
+      </div>
+      </div>`
+    }
+
+    // if (name2 == '') {
+    //   $('#errorMassage').text("Введите имя")
+    //   return false;
+    // } else if (contact2 == '') {
+    //   $('#errorMassage').text("Введите контактные данные")
+    //   return false;
+    // }
+    // $('#errorMassage').text('')
+
+    const letterData = {
+      to: 'goncharovfigma@gmail.com',
+      // to: 'justicejesus1237@gmail.com',
+      subject: 'Форма "Заказать проект2"',
+      text: 'yo',
+      html: createHtmlForEmail2()
+    }
+
+
+    $.ajax({
+      url: 'https://api.42.works/mailer',
+      type: 'POST',
+      cache: false,
+      data: JSON.stringify(letterData),
+      beforeSend: function () {
+        $('#sendForm').prop("disabled", true)
+      },
+      success: function (data) {
+        if (!data) {
+          alert('Произошла ошибка')
+        } else {
+          $('#form2').trigger("reset")
+        }
+        $('#sendForm').prop("disabled", false)
+      },
+      contentType: "application/json; charset=utf-8",
+    });
+  });
+
 })
-
-//переход конец 
-
-
-//слайды начало 
-
-
-//слайды конец 
